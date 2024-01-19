d3a5acf55cba5cc8f309fe135bd8503742470925
Abucia <72364028+MisterArt43@users.noreply.github.com>
2024-01-19 05:27:42 +0100

    Create FactionSetHome.ts

---
+++ scripts/Command/Faction/FactionSetHome.ts
@@ -0,0 +1,47 @@
+import { Player } from "@minecraft/server";
+import { Ply } from "../../Object/player/Ply";
+import { DB } from "../../Object/database/database";
+import { tellraw } from "../../Object/tool/tools";
+import { translate } from "../../lang";
+import { factionRank } from "../../Object/faction/Faction";
+import { addSubCommand, cmd_permission } from "../CommandManager";
+import { cmd_module } from "../../Object/database/db_map";
+
+addSubCommand(
+	"sethome",
+	"Set the faction home.",
+	`${globalThis.prefix}faction sethome`,
+	["sethome", "sh"],
+	cmd_module.faction,
+	cmd_permission.member,
+	true,
+	false,
+	factionSetHome,
+	[["faction", "f"]]
+)
+
+function factionSetHome(args: string[], player: Player, ply: Ply) {
+	const fac = DB.db_faction.get(ply.faction_name ?? "");
+
+	if (fac === undefined)
+		return tellraw(player, translate(ply.lang)?.error_cant_do_that ?? "no translation");
+	
+	//is Leader ?
+	if (fac.getRankFromName(ply.name) !== factionRank.Leader)
+		return tellraw(player, translate(ply.lang)?.error_cant_do_that ?? "no translation"); 
+
+	if (fac.isFhome) {
+		if (player.dimension.id === "minecraft:overworld") {
+			fac.remove_to_update_faction();
+			fac.setFhome(player.location);
+			fac.add_to_update_faction();
+			tellraw(player, translate(ply.lang, fac.Fhome!.x, fac.Fhome!.y, fac.Fhome!.z)?.faction_home ?? "no translation");
+		}
+		else {
+			tellraw(player, translate(ply.lang)?.error_faction_home ?? "no translation")
+		}
+	}
+	else {
+		tellraw(player, translate(ply.lang)?.error_faction_home_off ?? "no translation");
+	}
+}
