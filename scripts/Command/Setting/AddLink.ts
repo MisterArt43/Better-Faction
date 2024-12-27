13e2b86032e303306f233fb508b6e05966f33ab7
Abucia <72364028+MisterArt43@users.noreply.github.com>
2024-12-27 05:08:35 +0100

    typo

--- scripts/Command/Setting/AddLink.ts
+++ scripts/Command/Setting/AddLink.ts
@@ -21,79 +21,78 @@ addSubCommand(
 )
 
 async function link_item_to_command(args: string[], player: Player, ply: Ply) {
-    try{
+    try {
         const inventory = player.getComponent("inventory")!.container!;
-    let item = inventory.getItem(player.selectedSlotIndex);
+        let item = inventory.getItem(player.selectedSlotIndex);
 
-    let invItems = [];
-    for (let i = 0; i < 36; i++) {
-        if (inventory.getItem(i) !== undefined)
-        invItems.push(inventory.getItem(i)!.typeId);
-    }
+        let invItems = [];
+        for (let i = 0; i < 36; i++) {
+            if (inventory.getItem(i) !== undefined)
+                invItems.push(inventory.getItem(i)!.typeId);
+        }
 
-    let selectedItem: string | undefined = undefined;
+        let selectedItem: string | undefined = undefined;
 
-    if (item !== undefined) {
-        const res = await new BFActionFormData()
-        .title("Item Selection")
-        .body("Selected Item Detected, Do you want to link this item to a command?")
-        .button("Yes : " + item.typeId.match(/(?<=:).*/)![0])
-        .button("No (Select another from inventory)")
-        .show(player);
+        if (item !== undefined) {
+            const res = await new BFActionFormData()
+                .title("Item Selection")
+                .body("Selected Item Detected, Do you want to link this item to a command?")
+                .button("Yes : " + item.typeId.match(/(?<=:).*/)![0])
+                .button("No (Select another from inventory)")
+                .show(player);
 
-        if (res.canceled) return;
+            if (res.canceled) return;
 
-        if (res.selection === 0)
-            selectedItem = item.typeId;
-    }
+            if (res.selection === 0)
+                selectedItem = item.typeId;
+        }
 
-    if (selectedItem === undefined) {
-        const res = await new BFModalFormData()
-        .title("Item Selection")
-        .dropdown("Select an item", invItems)
-        .show(player);
+        if (selectedItem === undefined) {
+            const res = await new BFModalFormData()
+                .title("Item Selection")
+                .dropdown("Select an item", invItems)
+                .show(player);
 
-        if (res.canceled) return;
+            if (res.canceled) return;
 
-        log("slot : " + res.formValues![0]);
 
-        selectedItem = invItems[res.formValues![0] as number];
-        log("selectedItem : " + selectedItem);
-    }
+            selectedItem = invItems[res.formValues![0] as number];
+        }
 
-    if (DB.db_link.has(selectedItem)) return tellraw(player, "§cThis item is already linked to a command");
+        if (DB.db_link.has(selectedItem)) return tellraw(player, "§cThis item is already linked to a command");
 
-    const selectedCommand = await getCommand(ply, player);
+        const selectedCommand = await getCommand(ply, player);
 
-    if (selectedCommand === undefined || selectedCommand.length === 0) return;
+        if (selectedCommand === undefined || selectedCommand.length === 0) return;
 
-    const confirmForm = await new BFActionFormData()
-    .title("Link Confirmation")
-    .body("§eLinking §s" + selectedItem.match(/(?<=:).*/)![0] + "§e to §s" + selectedCommand + " §eCommand")
-    .button("§aConfirm")
-    .button("§cCancel")
-    .show(player);
+        const confirmForm = await new BFActionFormData()
+            .title("Link Confirmation")
+            .body("§eLinking §s" + selectedItem.match(/(?<=:).*/)![0] + "§e to §s" + selectedCommand + " §eCommand")
+            .button("§aConfirm")
+            .button("§cCancel")
+            .show(player);
 
-    if (confirmForm.canceled || confirmForm.selection === 1) return;
+        if (confirmForm.canceled || confirmForm.selection === 1) return;
 
-    CMDLinker.add_link(new CMDLinker(selectedCommand, selectedItem));} catch (error) { if (error instanceof Error) log(error.stack!); }
+        CMDLinker.add_link(new CMDLinker(selectedCommand, selectedItem));
+    } catch (error) { if (error instanceof Error) log(error.message + "\n§c" + error.stack!); }
 }
 
 
-async function getCommand(ply:Ply, player:Player) {
+async function getCommand(ply: Ply, player: Player) {
     try {
 
         let cursor: SubCommand = commands;
         let cmdStrings: string[] = ["all"];
-    
+
         let commandSelected = false;
-    
+
         while (!commandSelected && cursor instanceof Map) {
             const form = new BFActionFormData()
-            .title("Select a command")
-            .body("Current command: " + cmdStrings.join(" "))
-            .button("§aUse this command");
-    
+                .title("Select a command")
+                .body("Current command: " + cmdStrings.join(" "))
+                .button("§aUse this command");
+
             let cmds = getSubCommandPerAlias(cursor, ply, player, false);
             for (const cmd of cmds.seenCommands) {
                 form.button("§e" + cmd.command + " §o§7(Command)");
@@ -101,17 +100,17 @@ async function getCommand(ply:Ply, player:Player) {
             for (const cmd of cmds.seenSubCommands) {
                 form.button("§e" + cmd.keys[0] + " §o§7(SubCommand)");
             }
-    
+
             const dataForm = await form.show(player);
-    
+
             if (dataForm.canceled || dataForm.selection === undefined) return;
-    
+
             if (dataForm.selection === 0) {
                 commandSelected = true;
                 break;
             }
 
-    
+
             if (dataForm.selection > cmds.seenCommands.length) {
                 cursor = cursor.get(cmds.seenSubCommands[dataForm.selection - cmds.seenCommands.length - 1].keys[0]) as SubCommand;
                 cmdStrings.push(cmds.seenSubCommands[dataForm.selection - cmds.seenCommands.length - 1].keys[0]);
@@ -122,7 +121,6 @@ async function getCommand(ply:Ply, player:Player) {
             }
         }
 
-        log(cmdStrings.join(" "));
 
         if (cmdStrings.length > 1) cmdStrings.shift();
 
